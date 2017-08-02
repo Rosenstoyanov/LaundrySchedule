9096caed1beda50eaa2b5098893fcb3f160f68d7
Rosen Stoyanov <rosen.stoyanov@stanga.net>
2017-08-02 18:15:26 +0300

    init mongo

---
+++ app/db/index.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var fs = require('fs');
+var config = require(__base + 'app/config');
+var modelsPath = __base + 'app/db/models/';
+var path = require('path');
+
+var files = fs.readdirSync(modelsPath);
+
+mongoose.connect(config.db.url, function(err){
+    if(err){
+        console.log("Not connected to the database. :"+err);
+    } else{
+        console.log("Connected to MONGODB!");
+    }
+});
+
+module.exports.createModel = (name, schema) => mongoose.model(name, schema);
+
+module.exports.createSchema = schemaObj => mongoose.Schema(schemaObj);
+
+module.exports.models = (function() {
+    var files = fs.readdirSync(modelsPath);
+    return files.reduce((obj, fileName) => {
+        var name = path.parse(fileName).name;
+        obj[name.replace(/^./, name[0].toUpperCase())] = require(modelsPath + fileName);
+        return obj;
+    }, {});
+}());
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,6 +1,19 @@
 var router = require('express').Router();
+var User = require(__base + 'app/db').models.User;
 
 router.get("/", function(req, res){
   res.send("Hello Word")
 })
+
+router.post('/register', function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    var user = new User({ email: email, password: password });
+    user.save(function(err) {
+        if(err) return res.status(500).end();
+        res.status(200).end();
+    })
+});
+
+
 module.exports = router;
